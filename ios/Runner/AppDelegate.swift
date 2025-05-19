fe54688940b3118ba7c37369942279c858be164e
Pranay Suyash <pranay.suyash@gmail.com>
2025-05-19 17:33:42 +0530

    fix: update Info.plist, AppDelegate.swift for Google Sign-In, add LSApplicationQueriesSchemes, ensure Firebase config

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -15,4 +15,17 @@ import FirebaseCore
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+
+  override func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+    // Handle Google Sign-In callback
+    if let googleSignIn = NSClassFromString("GIDSignIn") as? NSObjectProtocol,
+       googleSignIn.responds(to: Selector(("sharedInstance"))) {
+      let sharedInstance = googleSignIn.perform(Selector(("sharedInstance")))?.takeUnretainedValue()
+      let handleSelector = Selector(("handleURL:"))
+      if let sharedInstance = sharedInstance, sharedInstance.responds(to: handleSelector) {
+        let _ = sharedInstance.perform(handleSelector, with: url)
+      }
+    }
+    return super.application(app, open: url, options: options)
+  }
 }
